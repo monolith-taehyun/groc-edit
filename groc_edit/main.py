a9d51c4f6c8c55b602bfde0ea19ea8f58ebedbf1
정태현 <taehyun.jung@monolith.co.kr>
2024-09-05 19:02:47 +0900

    feat: remove background
    - remove background
    - change extend to png

--- groc_edit/main.py
+++ groc_edit/main.py
@@ -1,15 +1,18 @@
 import cv2
-import dlib
 import os
+from rembg import remove
+from PIL import Image
+import numpy as np
 from utils import detect_face, crop_and_resize
 
-def process_images(input_folder, output_folder, target_size=(300, 400)):
+def process_images(input_folder, output_folder, target_size=(300, 400), remove_bg=True):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     
     for filename in os.listdir(input_folder):
-        print(f"Processing: {filename}")
         if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            print(f"Start Processing: {filename}")
+            
             image_path = os.path.join(input_folder, filename)
             image = cv2.imread(image_path)
             
@@ -23,13 +26,23 @@ def process_images(input_folder, output_folder, target_size=(300, 400)):
                 print(f"No face detected in: {filename}")
                 continue
             
+            print(f"Face detected in: {filename}")
+            
             processed = crop_and_resize(image, face, target_size)
             
-            output_path = os.path.join(output_folder, f"processed_{filename}")
+            if remove_bg:
+                print(f"Removing background from: {filename}")
+                pil_image = Image.fromarray(cv2.cvtColor(processed, cv2.COLOR_BGR2RGB))
+                
+                output = remove(pil_image)
+                
+                processed = cv2.cvtColor(np.array(output), cv2.COLOR_RGBA2BGRA)
+            
+            output_path = os.path.join(output_folder, f"processed_{filename}.png")
             cv2.imwrite(output_path, processed)
-            print(f"Processed: {filename} -> {output_path}")
+            print(f"End Processed: {filename} -> {output_path}")
 
 if __name__ == "__main__":
     input_folder = "input_images"
     output_folder = "output_images"
-    process_images(input_folder, output_folder)
+    process_images(input_folder, output_folder, remove_bg=True)
